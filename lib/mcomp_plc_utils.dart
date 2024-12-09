bbed883724c6c07698343dde0aaf0277ff5d42cd
Jakub Smolik <smolikja@protonmail.com>
2024-12-09 09:40:31 +0100

    feat: config fetcher

--- lib/mcomp_plc_utils.dart
+++ lib/mcomp_plc_utils.dart
@@ -1,7 +1,4 @@
-library mcomp_plc_utils;
+library;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'package:mcomp_plc_utils/src/config_fetcher/config_fetcher.dart'
+    show ConfigFetcher;
